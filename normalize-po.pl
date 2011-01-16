ebdceeba2996e96d0566c781f4700ae1432327c4
l <l@github.com>
2011-01-17 02:32:40 +0900

    訳語統一のためのスクリプトnormalize-po.pl の追加。 po ファイルの読み書きに Locale::Maketext::Extract モジュールを使っているので導入必須。
    使い方は以下。またはスクリプトの最後のあたり参照。
    
    手作業による編集が終了した後に、
    $ perl normalize-po.pl anope.ja_JP.po messages.po
    $ msgmerge --no-wrap messages.po anope.ja_JP.po > messages2.po
    $ msgfmt --statistics -c -v -o /dev/null messages2.po
    $ git commit -a
    これで訳語の統一化とコミットまで完了。
    
    統一化のルールを追加したい場合はスクリプトの
    while文の中で定義。今のところは la.l 用の
    normalize.pl で定義されていたルールと
    議題に挙がっていたルール。

---
+++ normalize-po.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Locale::Maketext::Lexicon {
+	_use_fuzzy => 1,
+	_allow_empty => 1,
+};
+use Locale::Maketext::Extract;
+
+my $lexicon = {};
+my $Ext = Locale::Maketext::Extract->new;
+$Ext->read_po($ARGV[0]);
+while (my ($key, $val) = each %{$Ext->lexicon}) {
+	my $_ = $val;
+	s/設定する。/設定します。/g;
+	s/サーバ(ー)?/サーバー/g;
+	s/オペレータ(ー)?/オペレーター/g;
+	s/ユーザ(ー)?/ユーザー/g;
+	s/キッカ(ー)?/キッカー/g;
+	s/ポリシ(ー)?/ポリシー/g;
+	s/エントリ(ー)?/エントリ/g;
+	s/デバック/デバッグ/g;
+	$lexicon->{$key} = $_;
+}
+$Ext->set_lexicon( { map Locale::Maketext::Extract::_maketext_to_gettext($_), %$lexicon } );
+$Ext->write_po($ARGV[1]);
+exit;
+__END__
+$ perl normalize-po.pl anope.ja_JP.po messages.po
+$ msgmerge --no-wrap messages.po anope.ja_JP.po > messages2.po
+$ msgfmt --statistics -c -v -o /dev/null messages2.po
+$ git commit -a
