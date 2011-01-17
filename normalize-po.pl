815b3de6fd7b7013bf60c7cac6761d9e37dcdc28
l <l@github.com>
2011-01-18 01:17:55 +0900

    半角英字の左右にスペースを置くルールを追加。正規化。

--- normalize-po.pl
+++ normalize-po.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use Encode;
 use Locale::Maketext::Lexicon {
 	_use_fuzzy => 1,
 	_allow_empty => 1,
@@ -14,8 +15,10 @@ while (my ($key, $val) = each %{$Ext->lexicon}) {
 	if ($key eq $val) {
 		$val = '';
 	}
-	my $_ = $val;
+	my $_ = spacer($val);
+	s/ICQ( )?ナンバー/ICQ 番号/g;
 	s/設定する。/設定します。/g;
+	s/相続人/後継者/g;
 	s/サーバ(ー)?/サーバー/g;
 	s/オペレータ(ー)?/オペレーター/g;
 	s/ユーザ(ー)?/ユーザー/g;
@@ -28,6 +31,19 @@ while (my ($key, $val) = each %{$Ext->lexicon}) {
 $Ext->set_lexicon( { map Locale::Maketext::Extract::_maketext_to_gettext($_), %$lexicon } );
 $Ext->write_po($ARGV[1]);
 exit;
+
+sub spacer
+{
+	my $str = shift;
+	my $east = qr/(?!\p{M})(?:\p{Han}|\p{Katakana}|\p{Hiragana})/;
+	my $west = qr/(?!\p{M})(?:\p{Latin}|\p{Greek}|\p{Cyrillic}|%)/;
+	$str = decode 'utf8', $str;
+	$str =~ s/($east)($west)/$1 $2/g;
+	$str =~ s/($west)($east)/$1 $2/g;
+	$str = encode 'utf8', $str;
+	return $str;
+}
+
 __END__
 $ perl normalize-po.pl anope.ja_JP.po messages.po
 $ msgmerge --no-wrap messages.po anope.ja_JP.po > messages2.po
